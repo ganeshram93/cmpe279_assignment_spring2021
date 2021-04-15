cf2c4ab903bad341455faac6a21f3c69ce6808ee
ganeshram93 <ganeshram2493@gmail.com>
2021-04-14 19:05:21 -0700

    Made following changes:
    1. Located split between socket setup and data processing
    2. Created a user in local machine "user1" and dropped the privileges to it using setgid() and setuid()
    3. Child processes the data sent by the client
    4. Parent waits for child to exits. Once the child processes the data from the client, it returns to parent
    
    Signed-off-by: ganeshram93 <ganeshram2493@gmail.com>

--- server.c
+++ server.c
@@ -1,4 +1,4 @@
-// Server side C/C++ program to demonstrate Socket programming
+/* Server side C/C++ program to demonstrate Socket programming */
 
 #include <unistd.h>
 #include <stdio.h>
@@ -6,8 +6,15 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pwd.h>
+#include <grp.h>
 
 #define PORT 80
+#define NEWLY_CREATED_USER "user1"
+
 int main(int argc, char const *argv[])
 {
     int server_fd, new_socket, valread;
@@ -16,17 +23,19 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address);
     char buffer[102] = {0};
     char *hello = "Hello from server";
+    pid_t process_pid, wait_pid;
+    int status = 0, realuid = 0, euid = 0, realgid = 0, egid = 0;
 
     printf("execve=0x%p\n", execve);
 
-    // Creating socket file descriptor
+    /* Creating socket file descriptor */
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Attaching socket to port 80
+    /* Attaching socket to port 80 */
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                                                   &opt, sizeof(opt)))
     {
@@ -37,27 +46,97 @@ int main(int argc, char const *argv[])
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Forcefully attaching socket to the port 80
+    /* Forcefully attaching socket to the port 80 */
+
     if (bind(server_fd, (struct sockaddr *)&address,
                                  sizeof(address)) < 0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0)
+
+    printf("=====In parent process=====\n");
+    realuid = getuid();
+    euid = geteuid();
+    realgid = getgid();
+    egid = getegid();
+    printf("Parent Real UID: %d\n", realuid);
+    printf("Parent Effective UID: %d\n", euid);
+    printf("Parent Real GID: %d\n", realgid);
+    printf("Parent Effective GID: %d\n", egid);
+
+    /* Located the split: Data processing logic begins below */
+    if ((process_pid = fork()) < 0)
     {
-        perror("listen");
-        exit(EXIT_FAILURE);
+        printf("Error: Unable to fork\n");
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                       (socklen_t*)&addrlen)) < 0)
+
+    if(process_pid == 0)
     {
-        perror("accept");
-        exit(EXIT_FAILURE);
+	struct group *grp = getgrnam(NEWLY_CREATED_USER);
+	struct passwd *pwd = getpwnam(NEWLY_CREATED_USER);
+	int groupid;
+	int userid;
+
+	if (grp == NULL)
+	{
+            printf("Error: No group ID found for user %s. Exiting\n", NEWLY_CREATED_USER);
+            exit(EXIT_FAILURE);
+        }
+	if (pwd == NULL)
+	{
+            printf("Error: No user ID found for user %s. Exiting\n", NEWLY_CREATED_USER);
+            exit(EXIT_FAILURE);
+        }
+
+	groupid = grp->gr_gid;
+	userid = pwd->pw_uid;
+
+        printf("\n=====In child process=====\nGID = %d | UID = %d\n", groupid, userid);
+
+	/* Drop privileges by running the process with given GID and UID */
+        if (setgid(groupid) != 0)
+        {
+            printf("Error: setgid failed. Unable to drop group privileges. Exiting\n");
+            exit(EXIT_FAILURE);
+        }
+        if (setuid(userid) != 0)
+        {
+            printf("Error: setuid failed. Unable to drop group privileges. Exiting\n");
+            exit(EXIT_FAILURE);
+        }
+
+        realuid = getuid();
+        euid = geteuid();
+        realgid = getgid();
+        egid = getegid();
+        printf("Child Real UID: %d\n", realuid);
+        printf("Child Effective UID: %d\n", euid);
+        printf("Child Real GID: %d\n", realgid);
+        printf("Child Effective GID: %d\n", egid);
+
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("Error: Unable to listen. Exiting");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                           (socklen_t*)&addrlen)) < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("\nReceived message from socket:");
+        valread = read(new_socket, buffer, 1024);
+        printf("\n%s\n", buffer);
+        send(new_socket, hello, strlen(hello), 0);
+        printf("Hello message sent\n");
+	exit(0);
     }
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
+
+    /* Wait for child process to exit */
+    while ((wait_pid = wait(&status)) > 0);
+    printf("\nChild process has exited\n");
     return 0;
 }
