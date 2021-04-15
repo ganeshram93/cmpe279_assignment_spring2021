8c5e8c6fc547a2652f10d8478ad2a1b3f0b17a84
ganeshram93 <ganeshram2493@gmail.com>
2021-04-14 17:14:59 -0700

    Made following changes:
    1. Removed extra whitespaces in server.c and re-formatted few lines of code.
    
    Signed-off-by: ganeshram93 <ganeshram2493@gmail.com>

--- server.c
+++ server.c
@@ -35,11 +35,11 @@ int main(int argc, char const *argv[])
     }
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address.sin_port = htons(PORT);
 
     // Forcefully attaching socket to the port 80
     if (bind(server_fd, (struct sockaddr *)&address,
-                                 sizeof(address))<0)
+                                 sizeof(address)) < 0)
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -50,14 +50,14 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                       (socklen_t*)&addrlen))<0)
+                       (socklen_t*)&addrlen)) < 0)
     {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
+    valread = read(new_socket, buffer, 1024);
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
     printf("Hello message sent\n");
     return 0;
 }
